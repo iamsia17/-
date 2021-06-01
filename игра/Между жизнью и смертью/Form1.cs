1ee49d193758ea566d557420e1d8b52134d64559
iamsia17 <stud0000222446@study.utmn.ru>
2021-06-01 19:14:02 +0500

    Сделали меню

--- игра/Между жизнью и смертью/Form1.cs
+++ игра/Между жизнью и смертью/Form1.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Между_жизнью_и_смертью
 {
     public partial class Form1 : Form
     {
+        Thread th;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            th = new Thread(open);
+            th.SetApartmentState(ApartmentState.STA);
+            th.Start();
+        }
+        public void open(object obj)
+        {
+            Application.Run(new Form2());
         }
     }
 }
---
+++ игра/Между жизнью и смертью/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Между_жизнью_и_смертью
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox38_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
